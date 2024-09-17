7c3fd055a5773c166873ee0418005763c4c085e1
Wonbin Kim <kwb0711@gmail.com>
2024-09-17 22:46:52 +0900

    .

--- exp/exp1.py
+++ exp/exp1.py
@@ -15,6 +15,36 @@ class Exp:
         
         pass
     
+    def get_weighted_sum(self, user_id, product_id):
+        
+        res = self.cursor.execute(
+                f"""
+                SELECT 
+                    SUM(ps.similarity * rtrain.rating) AS up,
+                    SUM(ps.similarity) AS down
+                FROM
+                    (
+                        SELECT 
+                            product_id,
+                            rating
+                        FROM reviews_train
+                        WHERE user_id = '{user_id}'
+                    ) rtrain 
+                        INNER JOIN pearson_sim ps
+                            ON rtrain.product_id = ps.product_id_2 
+                WHERE
+                    ps.product_id_1 = '{product_id}';
+                """       
+            )
+        
+        result = res.fetchone()
+        
+        if len(result) == 0 or result[0] is None or result[1] is None or result[1] == 0.0:
+            return None
+        
+        return result[0] / result[1]
+        
+        
     def test(self, user_id, product_id):
         """_summary_
 
@@ -24,4 +54,4 @@ class Exp:
             
         1. get list of products, which is rated by the user, user_id
         """
-        
+        return self.get_weighted_sum(user_id, product_id)
--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 import sqlite3
 from exp.exp1 import Exp
+import math
+from tqdm import tqdm
 
 def test():
     """
     
     """
-    pass
+    db_connection = sqlite3.connect("reviews.db")
+    exp = Exp(db_connection=db_connection)
+    
+    cursor = db_connection.cursor()
+    test_set = cursor.execute("SELECT product_id, user_id, rating FROM reviews_test").fetchall()
+    
+    error = 0.0
+    count = 0
+    
+    for product_id, user_id, rating in tqdm(test_set):
+        
+        tmp = exp.test(user_id, product_id)
+        if tmp is not None:
+            error += math.sqrt((tmp - rating) ** 2) / rating
+            count += 1
+            
+    error = error / count
+    print(f"error: {error}, count = {count}")
 
 def main():
     """
@@ -15,11 +34,8 @@ def main():
     4. using metric, get the score of the model
     
     """
+    test()
     
-    db_connection = sqlite3.connect("reviews.db")
-    exp = Exp(db_connection=db_connection)
-    
-    print(exp.get_pearson_coeff('B09XWYG6X1', 'B0BXDLF8TW'))
     
 if __name__ == "__main__":
     main()
